9ac5e8acff3f6459461e9488669da7419ab1e7e8
Wilson <33164598+WilsontheWolf@users.noreply.github.com>
2019-06-19 18:44:59 -0600

    Create sound.user.js

---
+++ sound.user.js
@@ -0,0 +1,88 @@
+// ==UserScript==
+// @name         BuzzIn.live Custom Sound Player
+// @version      0.1
+// @description  Custom sounds on BuzzIn.live!
+// @author       WilsontheWolf
+// @match        *buzzin.live/play
+// @grant        none
+// ==/UserScript==
+var opttxt = ['Custom',
+    'Deafult',
+    'Deja Vu',
+    'Dear Sister',
+    'Fus Ro Dah',
+    'GameCube Logo',
+    'Sexy Sax',
+    'Leeroy Jenkins',
+    'That was easy!',
+    'Flower Garden',
+    'Seinfeld Theme',
+    'Shooting Stars'
+]
+var optval = ['custom',
+    'buzz.mp3',
+    'https://www.myinstants.com//media/sounds/deja-vu.mp3',
+    'https://www.myinstants.com/media/sounds/SNL_Digital_Short_-_Dear_Sister_1.mp3',
+    'https://www.myinstants.com/media/sounds/fus-ro-dah.mp3',
+    'https://www.myinstants.com/media/sounds/gamecube_intro.mp3',
+    'https://www.myinstants.com/media/sounds/george-micael-wham-careless-whisper-1.mp3',
+    'https://www.myinstants.com/media/sounds/leroy.swf.mp3',
+    'https://www.myinstants.com/media/sounds/that_was_easy.mp3',
+    'https://www.myinstants.com/media/sounds/flower-garden-alert.mp3',
+    'https://www.myinstants.com/media/sounds/seinfeld-theme_1.mp3',
+    'https://www.myinstants.com/media/sounds/untitled-1_qPskhfn.mp3',
+    'https://www.myinstants.com',
+    'https://www.myinstants.com',
+    'https://www.myinstants.com',
+]
+let i
+var input = document.createElement("input");
+var button = document.createElement("input");
+var options = document.createElement("select")
+for (i = 0; i < opttxt.length; i++) {
+    let opt = document.createElement("option");
+    opt.value = optval[i];
+    opt.text = opttxt[i];
+    options.add(opt, null);
+}
+options.setAttribute("style", "font-size:18px;position:absolute;top:5px;right:40px;");
+options.onchange = change
+document.body.appendChild(options);
+input.type = "text";
+input.value = "https://www.myinstants.com//media/sounds/deja-vu.mp3";
+input.setAttribute("style", "font-size:18px;position:absolute;top:30px;right:107px;");
+document.body.appendChild(input);
+button.type = "button";
+button.value = "Apply";
+button.onclick = changeSound(input.value);
+button.setAttribute("style", "font-size:18px;position:absolute;top:30px;right:40px;");
+document.body.appendChild(button);
+
+function changeSound(sound) {
+    buzzSound = new Howl({
+        src: [sound],
+        rate: 1.1
+    })
+    document.getElementById("mute").checked = true
+}
+
+function customSound(sound) {
+    changeSound(sound)
+    console.log(`BuzzIn.live Custom Sound Player changed the buzzer to a custom sound (${sound})`)
+}
+
+function change() {
+    let value = optval[options.selectedIndex]
+    if (value == 'custom') {
+        input.disabled = false
+        button.disabled = false
+    } else {
+        input.disabled = true
+        button.disabled = true
+    }
+    if (value != 'custom') {
+        changeSound(value)
+        console.log(`BuzzIn.live Custom Sound Player changed the buzzer to ${opttxt[options.selectedIndex]} (${value})`)
+    }
+
+}
